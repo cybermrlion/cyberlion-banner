f3b2941d39430bbb3508d5d2f6ddffc14aea8f30
cybermrlion <155144344+cybermrlion@users.noreply.github.com>
2023-12-29 15:22:27 +0530

    Create cyber lion.sh

---
+++ cyber lion.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+#color
+y="\e[1;33m"
+r="\e[1;31m"
+b="\e[1;34m"
+
+echo -e $y "hi cyber mr lion."
+echo -e $r "Enter your username:"
+read username
+echo -e $b "you have successfully logged in $username"
